2233ee636261d2faa0657d5225e615cbf63a897e
Magnus <pedmagnus@gmail.com>
2022-06-18 10:48:42 -0300

    fix: adding no cors headers

--- src/main/kotlin/br/magnus/readeble/exceptions/ServiceUnavailableErrorExceptionHandler.kt
+++ src/main/kotlin/br/magnus/readeble/exceptions/ServiceUnavailableErrorExceptionHandler.kt
@@ -7,7 +7,8 @@ import javax.ws.rs.ext.Provider
 @Provider
 class ServiceUnavailableErrorExceptionHandler : ExceptionMapper<ServiceUnavailableException> {
     override fun toResponse(exception: ServiceUnavailableException): Response {
-        return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(exception.message).build()
+        return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(exception.message)
+            .header("Access-Control-Allow-Origin", "*").build()
     }
 
 }
--- src/main/kotlin/br/magnus/readeble/exceptions/UnprocessableEntityExceptionHandler.kt
+++ src/main/kotlin/br/magnus/readeble/exceptions/UnprocessableEntityExceptionHandler.kt
@@ -7,7 +7,7 @@ import javax.ws.rs.ext.Provider
 @Provider
 class UnprocessableEntityExceptionHandler : ExceptionMapper<UnprocessableEntityException> {
     override fun toResponse(exception: UnprocessableEntityException): Response {
-        return Response.status(422).entity(exception.message).build()
+        return Response.status(422).entity(exception.message).header("Access-Control-Allow-Origin", "*").build()
     }
 
 }
